d0096e77639f493f1d48ccc3a9cf68c03dab2781
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 02:06:12 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,5 @@
 
-#array= ["Tim Jones", "Tom Smith", "Jim Campagno"]
+array= ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect (array)
   i = 0
@@ -13,10 +13,4 @@ end
 
 my_collect(array) do |i|
   i.split(" ").first
-  puts i
-end
-
-my_collect(array) do |i|
-  i.lowcase
-  puts i
 end
