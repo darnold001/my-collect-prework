5c0860b3fbca258b5eeae89ef2b02afd821e5dfd
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 01:41:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,12 @@
-def my_collect (cond)
+def my_collect (array)
+  i = 0
 
-  while i > cond.length do
-    yield my_collect
+  while i < array.length
+    yield (array[i])
     i = i + 1
   end
   cond
+end
+my_collect(array) do i
+  i.split(" ").first
 end
