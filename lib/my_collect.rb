ec1526831dde494c74d8aa643df2cec458bebf85
Devin Arnold <dbarnold321@gmail.com>
2019-05-16 02:29:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,18 +3,19 @@ array= ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect (array)
   i = 0
+  result = []
 
   while i < array.length
-    yield (array[i])
+  array <<  yield (array[i])
     i = i+1
   end
-  array
+  result
 end
 
-my_collect(array) do |i|
-  i.split(" ").first
-end
+#my_collect(array) do |i|
+#  i.split(" ").first
+#end
 
-my_collect(array) do |i|
-  i.upcase
-end
+#my_collect(array) do |i|
+#  i.upcase
+#end
